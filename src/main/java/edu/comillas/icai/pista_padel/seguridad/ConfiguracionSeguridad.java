3c087126126d06572bc6a36d4cf8a085fb70c850
ignaciosanzsaizz <sanzsaizignacio@gmail.com>
2026-02-18 17:10:54 +0100

    Fix de errores de compilacion 3

--- src/main/java/edu/comillas/icai/pista_padel/seguridad/ConfiguracionSeguridad.java
+++ src/main/java/edu/comillas/icai/pista_padel/seguridad/ConfiguracionSeguridad.java
@@ -3,7 +3,9 @@ package edu.comillas.icai.pista_padel.seguridad;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,22 +15,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity // para @PreAuthorize que usarán las otras personas
 @EnableWebSecurity
-
+@EnableMethodSecurity
 public class ConfiguracionSeguridad {
 
     @Bean
     public SecurityFilterChain filtroSeguridad(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        // públicos
                         .requestMatchers(HttpMethod.GET, "/pistaPadel/health").permitAll()
                         .requestMatchers(HttpMethod.POST, "/pistaPadel/auth/register").permitAll()
-                        // el resto, autenticado
                         .anyRequest().authenticated()
                 );
 
@@ -36,9 +37,14 @@ public class ConfiguracionSeguridad {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public PasswordEncoder codificadorPassword() {
         return new BCryptPasswordEncoder();
     }
 }
-
 
